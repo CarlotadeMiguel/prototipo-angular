7a7dd2ddb390f0e21131489897acf3af894a132b
CarlotadeMiguel <carlota.demiguel@tajamar365.com>
2025-04-22 18:47:31 +0200

    typeof

--- src/app/add-task/add-task.component.ts
+++ src/app/add-task/add-task.component.ts
@@ -9,33 +9,35 @@ import { FormsModule } from '@angular/forms';
   styleUrls: ['./add-task.component.css'],
   imports: [CommonModule, FormsModule]
 })
- 
+
 export class AddTaskComponent {
   userDetails = {
     name: ''
   };
- 
+
   constructor() {
-    // Verificar si ya hay datos en localStorage y cargarlos si es así
-    const storedData = localStorage.getItem('datas');
-    if (storedData) {
-      this.userDetails = JSON.parse(storedData);
+    if (typeof window !== 'undefined') {
+      // Verificar si ya hay datos en localStorage y cargarlos si es así
+      const storedData = localStorage.getItem('datas');
+      if (storedData) {
+        this.userDetails = JSON.parse(storedData);
+      }
     }
   }
- 
+
   submitForm(form: any): void {
     if (form.valid) {
       console.log('Form data:', this.userDetails);
- 
+
       // Obtener los datos previos del localStorage
       let storedData: any[] = JSON.parse(localStorage.getItem('datas') || '[]');
- 
+
       // Agregar los nuevos detalles del usuario
-      storedData.push(this.userDetails);
- 
-      // Guardar el array actualizado en localStorage
-      localStorage.setItem('datas', JSON.stringify(storedData));
- 
+      storedData.push(this.userDetails); 
+      if (typeof window !== 'undefined') {
+        // Guardar el array actualizado en localStorage
+        localStorage.setItem('datas', JSON.stringify(storedData));
+      }
       console.log('Data saved:', storedData);
     }
   }
